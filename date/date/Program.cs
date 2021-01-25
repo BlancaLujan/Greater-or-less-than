3904ff9cbc53f7746c702e8f0e2066d47f46008f
BlancaLujan <77300324+BlancaLujan@users.noreply.github.com>
2021-01-25 13:29:01 -0700

    Greater or less than

---
+++ date/date/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace date
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            DateTime myDateTimeVariable = new DateTime(2005, 8, 2);
+            DateTime myDateTimeVariable2 = DateTime.Now;
+
+            int result = DateTime.Compare(myDateTimeVariable, myDateTimeVariable2);
+
+
+            Console.WriteLine(result);
+            {
+                if (myDateTimeVariable < myDateTimeVariable2)
+                {
+                    Console.WriteLine("Less than");
+                }
+                else
+                {
+                    Console.WriteLine("Greater than");
+                }
+            }
+        }
+    }
+}
+
+    
+
+
